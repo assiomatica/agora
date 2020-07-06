13869a79faebcfee9dc5a454e3bef4cb59f83b5e
CASULA, Antonio <antonio.casula@aerztekasse.ch>
2020-07-06 10:38:45 +0200

    start

---
+++ precache-manifest.a86e86358646f7196f79b2eefe53fcff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3391db0cb6ede2338e4c7a6a960ea87d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97a4578a10f7c811ca34",
+    "url": "./static/css/main.177c6ac2.chunk.css"
+  },
+  {
+    "revision": "f9afca4c6f6aa616a1d0",
+    "url": "./static/js/2.aa8f19e7.chunk.js"
+  },
+  {
+    "revision": "97a4578a10f7c811ca34",
+    "url": "./static/js/main.0b456220.chunk.js"
+  },
+  {
+    "revision": "ed8dc811513c8cc872e1",
+    "url": "./static/js/runtime~main.d8ca0601.js"
+  },
+  {
+    "revision": "db7ddd5a4c6bcd2cbe349110b51daaeb",
+    "url": "./static/media/avatar.db7ddd5a.png"
+  },
+  {
+    "revision": "3165c186e5879ca5ee9dfd119953d245",
+    "url": "./static/media/icon-audience-inactived.3165c186.png"
+  },
+  {
+    "revision": "2620e52585f92615e6ab5ee899c119f6",
+    "url": "./static/media/icon-audience.2620e525.png"
+  },
+  {
+    "revision": "25174b6ec7aee7a101e68cdf6d7143a8",
+    "url": "./static/media/icon-host-inactived.25174b6e.png"
+  },
+  {
+    "revision": "6baf9f5c788826fe3e42804dec3b89ab",
+    "url": "./static/media/icon-host.6baf9f5c.png"
+  },
+  {
+    "revision": "41c0dce84904b9e62385f51a888a73c1",
+    "url": "./static/media/logo-open-live-gray.41c0dce8.png"
+  },
+  {
+    "revision": "1cccf552e25f6614e1b734b29eb4b94d",
+    "url": "./static/media/logo-open-live.1cccf552.png"
+  }
+]);
